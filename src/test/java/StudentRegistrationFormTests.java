7a4d71c986539b59fc67cfce8dbe07397a4c94e0
RenataFatykhov <renata.musenova@gmail.com>
2026-06-17 18:38:28 +0300

    added parameterized test

--- src/test/java/StudentRegistrationFormTests.java
+++ src/test/java/StudentRegistrationFormTests.java
@@ -1,7 +1,8 @@
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
@@ -9,9 +10,24 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class StudentRegistrationFormTests extends TestBase {
 
-    @Test
-    @DisplayName("Заполнение всех полей формы")
-    public void successfulStudentRegistrationFormTest() {
+    @CsvFileSource(resources = "/test_data/StudentRegistrationFormTests.csv", numLinesToSkip = 1)
+    @ParameterizedTest(name = "Заполнение всех полей формы")
+    public void successfulStudentRegistrationFormTest(
+            String name,
+            String surname,
+            String email,
+            String gender,
+            String phoneNumber,
+            int dayOfBirth,
+            String monthOfBirth,
+            int yearOfBirth,
+            String subjects,
+            String hobbies,
+            String address,
+            String state,
+            String city
+    ) {
+
         open("/automation-practice-form");
         executeJavaScript("""
                 document.getElementById('fixedban')?.remove();
@@ -19,48 +35,49 @@ public class StudentRegistrationFormTests extends TestBase {
                 document.querySelectorAll('[class*="ad"], [class*="banner"], iframe').forEach(el => el.remove());
                 """);
 
-        $("[id=firstName]").setValue("Renata");
-        $("[id=lastName]").setValue("Fatykhova");
-        $("[id=userEmail]").setValue("ren.fatykhova@gmail.com");
-        $("[id=genterWrapper] [value=Female]").click();
-        $("[id=userNumber]").setValue("1234567890");
+        $("[id=firstName]").setValue(name);
+        $("[id=lastName]").setValue(surname);
+        $("[id=userEmail]").setValue(email);
+        $("[id=genterWrapper] [value=" + gender + "]").click();
+        $("[id=userNumber]").setValue(phoneNumber);
 
         $("[id=dateOfBirthInput]").click();
 
-        $(".react-datepicker__month-select").selectOption("November");
-        $(".react-datepicker__year-select").selectOption("1997");
-        $(".react-datepicker__day.react-datepicker__day--004").click();
+        $(".react-datepicker__month-select").selectOption(monthOfBirth);
+        $(".react-datepicker__year-select").selectOption(yearOfBirth);
+        $(".react-datepicker__day.react-datepicker__day--00" + dayOfBirth).click();
 
-        $(".subjects-auto-complete__input").setValue("Maths");
+        $(".subjects-auto-complete__input").setValue(subjects);
         $(".subjects-auto-complete__option").click();
-        $("[id=hobbiesWrapper]").$(byText("Reading")).click();
+        $("[id=hobbiesWrapper]").$(byText(hobbies)).click();
 
-        $("[id=uploadPicture]").uploadFromClasspath(String.valueOf("for_test.jpeg"));
+        $("[id=uploadPicture]").uploadFromClasspath("for_test.jpeg");
 
 
-        $("[id=currentAddress]").setValue("The best street 2");
+        $("[id=currentAddress]").setValue(address);
 
-        $("[id=react-select-3-input]").setValue("NCR").pressEnter();
-        $("[id=react-select-4-input]").setValue("Delhi").pressEnter();
+        $("[id=react-select-3-input]").setValue(state).pressEnter();
+        $("[id=react-select-4-input]").setValue(city).pressEnter();
 
         executeJavaScript("arguments[0].click();", $("button[id=submit]"));
 
         $(".modal-content").shouldBe(visible);
         $("[id=example-modal-sizes-title-lg]").shouldHave(text("Thanks for submitting the form"));
 
-        $(".table-responsive").$(byText("Student Name")).parent().
-                shouldHave(text("Renata Fatykhova"));
-        $(".table-responsive").$(byText("Student Email")).
-                parent().shouldHave(text("ren.fatykhova@gmail.com"));
-        $(".table-responsive").$(byText("Gender")).parent().shouldHave(text("Female"));
-        $(".table-responsive").$(byText("Mobile")).parent().shouldHave(text("1234567890"));
-        $(".table-responsive").$(byText("Date of Birth")).
-                parent().shouldHave(text("4 November,1997"));
-        $(".table-responsive").$(byText("Subjects")).parent().shouldHave(text("Maths"));
-        $(".table-responsive").$(byText("Hobbies")).parent().shouldHave(text("Reading"));
+        $(".table-responsive").$(byText("Student Name")).parent()
+                .shouldHave(text(name + " " + surname));
+        $(".table-responsive").$(byText("Student Email"))
+                .parent().shouldHave(text(email));
+        $(".table-responsive").$(byText("Gender")).parent().shouldHave(text(gender));
+        $(".table-responsive").$(byText("Mobile")).parent().shouldHave(text(phoneNumber));
+        $(".table-responsive").$(byText("Date of Birth"))
+                .parent().shouldHave(text(dayOfBirth + " " + monthOfBirth + "," + yearOfBirth));
+        $(".table-responsive").$(byText("Subjects")).parent().shouldHave(text(subjects));
+        $(".table-responsive").$(byText("Hobbies")).parent().shouldHave(text(hobbies));
         $(".table-responsive").$(byText("Picture")).parent().shouldHave(text("for_test.jpeg"));
-        $(".table-responsive").$(byText("Address")).parent().shouldHave(text("he best street 2"));
-        $(".table-responsive").$(byText("State and City")).parent().shouldHave(text("NCR Delhi"));
+        $(".table-responsive").$(byText("Address")).parent().shouldHave(text(address));
+        $(".table-responsive").$(byText("State and City"))
+                .parent().shouldHave(text(state + " " + city));
 
         $("[id=closeLargeModal]").shouldBe(Condition.clickable);
 
@@ -177,10 +194,8 @@ public class StudentRegistrationFormTests extends TestBase {
         $("[id=genterWrapper] [value=Male]").click();
         $("[id=genterWrapper] [value=Female]").click();
 
-        $("[id=genterWrapper] [value=Male]").shouldHave
-                (cssValue("background-color", "rgba(255, 255, 255, 1)"));
-        $("[id=genterWrapper] [value=Female]").shouldHave
-                (cssValue("background-color", "rgba(13, 110, 253, 1)"));
+        $("[id=genterWrapper] [value=Male]").shouldBe(not(focused));
+        $("[id=genterWrapper] [value=Female]").shouldBe(focused);
     }
     @Test
     @DisplayName("Модальное окно исчезает после нажатия на кнопку закрытия")
