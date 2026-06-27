ba186482ba28f771c1f40a04e6a53ab188fa2353
RenataFatykhov <renata.musenova@gmail.com>
2026-06-27 21:07:15 +0300

    deleted unused imports

--- src/test/java/StudentRegistrationFormTests.java
+++ src/test/java/StudentRegistrationFormTests.java
@@ -1,16 +1,12 @@
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.*;
-
-import java.util.stream.Stream;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
-
 import static testdata.TestData.*;
+import static testdata.TestData.name;
 
 public class StudentRegistrationFormTests extends TestBase {
 
