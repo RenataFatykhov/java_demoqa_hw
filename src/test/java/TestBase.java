d75a3d3f3bb74a0b589694889f16bcaf4f06e0d1
RenataFatykhov <renata.musenova@gmail.com>
2026-07-19 19:21:32 +0300

    some fixes

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,15 +1,13 @@
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import pages.StudentRegistrationFormPage;
-import pages.TextBoxPage;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestBase {
     @BeforeAll
     static void setUp() {
+        Configuration.headless = true;
         Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
