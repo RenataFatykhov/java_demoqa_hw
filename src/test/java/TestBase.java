0a9791d4b99a05c55936961032560877400ab6cf
RenataFatykhov <renata.musenova@gmail.com>
2026-07-25 01:09:44 +0300

    added attachments

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,5 +1,6 @@
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -23,6 +24,11 @@ public class TestBase {
 
     @AfterEach
     void tearDown() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
         closeWebDriver();
     }
+
 }
