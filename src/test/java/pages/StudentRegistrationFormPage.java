c03cc4768744a91d0a2e3f7913255bd6779e0991
RenataFatykhov <renata.musenova@gmail.com>
2026-07-25 00:08:23 +0300

    added test steps

--- src/test/java/pages/StudentRegistrationFormPage.java
+++ src/test/java/pages/StudentRegistrationFormPage.java
@@ -2,6 +2,7 @@ package pages;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 import pages.components.CalendarComponent;
 import pages.components.ResultTableComponent;
 
@@ -11,7 +12,6 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class StudentRegistrationFormPage {
 
-    // Elements
     CalendarComponent calendar = new CalendarComponent();
     ResultTableComponent table = new ResultTableComponent();
 
@@ -31,12 +31,13 @@ public class StudentRegistrationFormPage {
     private final SelenideElement correctMessage = $("#example-modal-sizes-title-lg");
     private final SelenideElement modalButton = $("#closeLargeModal");
 
-    // Actions
+    @Step("Открыть страницу /automation-practice-form")
     public StudentRegistrationFormPage openPage() {
         open("/automation-practice-form");
         return this;
     }
 
+    @Step("Подготовить страницу")
     public StudentRegistrationFormPage preparePage() {
         executeJavaScript("""
                 document.getElementById('fixedban')?.remove();
@@ -46,104 +47,124 @@ public class StudentRegistrationFormPage {
         return this;
     }
 
+    @Step("Ввод имени \"{value}\"")
     public StudentRegistrationFormPage typeName(String value) {
         nameInput.setValue(value);
         return this;
     }
 
+    @Step("Ввод фамилии \"{value}\"")
     public StudentRegistrationFormPage typeSurname(String value) {
         surnameInput.setValue(value);
         return this;
     }
 
+    @Step("Ввод email \"{value}\"")
     public StudentRegistrationFormPage typeEmail(String value) {
         emailInput.setValue(value);
         return this;
     }
 
+    @Step("Выбор gender \"{value}\"")
     public StudentRegistrationFormPage setGender(String value) {
         genderRadio.$("[value=" + value + "]").click();
         return this;
     }
 
+    @Step("Определение состояния радиокнопок \"{male}\" и \"{female}\"")
     public StudentRegistrationFormPage checkStateOfGenderRadio(String male, String female) {
         genderRadio.$("[value=" + male + "]").shouldNotBe(selected);
         genderRadio.$("[value=" + female + "]").shouldBe(selected);
         return this;
     }
 
+    @Step("Ввод номера телефона \"{value}\"")
     public StudentRegistrationFormPage typeUserNumber(String value) {
         userNumberInput.setValue(value);
         return this;
     }
 
+    @Step("Установка дня \"{day}\", месяца \"{month}\" и года рождения \"{year}\"")
     public StudentRegistrationFormPage setDateOfBirth(int day, String month, String year) {
         dateOfBirthInput.click();
         calendar.setDate(day, month, year);
         return this;
     }
 
+    @Step("Ввод предмета \"{value}\"")
     public StudentRegistrationFormPage typeSubject(String value) {
         subjectInput.setValue(value).pressEnter();
         return this;
     }
 
+    @Step("Установка хобби \"{value}\"")
     public StudentRegistrationFormPage setHobbies(String value) {
         hobbiesCheckbox.$(byText(value)).click();
         return this;
     }
 
+    @Step("Загрузка файла \"{value}\"")
     public StudentRegistrationFormPage uploadPicture(String value) {
         imageUploader.uploadFromClasspath(value);
         return this;
     }
 
+    @Step("Ввод адреса \"{value}\"")
     public StudentRegistrationFormPage typeAddress(String value) {
         addressInput.setValue(value).pressEnter();
         return this;
     }
 
+    @Step("Выбор штата \"{value}\"")
     public StudentRegistrationFormPage setState(String value) {
         stateSelect.setValue(value).pressEnter();
         return this;
     }
 
+    @Step("Выбор города \"{value}\"")
     public StudentRegistrationFormPage setCity(String value) {
         citySelect.setValue(value).pressEnter();
         return this;
     }
 
+    @Step("Выбор штата \"{state}\" и города \"{city}\"")
     public StudentRegistrationFormPage setStateAndCity(String state, String city) {
         setState(state);
         setCity(city);
         return this;
     }
 
+    @Step("Клик по кнопке подтверждения")
     public StudentRegistrationFormPage clickSubmitButton() {
         executeJavaScript("arguments[0].click();", $("button[id=submit]"));
         return this;
     }
 
+    @Step("Проверка видимости модального окна: отображается")
     public StudentRegistrationFormPage checkVisibleModal() {
         resultModal.shouldBe(visible);
         return this;
     }
 
+    @Step("Проверка видимости модального окна: не отображается")
     public StudentRegistrationFormPage checkNotVisibleModal() {
         resultModal.shouldNotBe(visible);
         return this;
     }
 
+    @Step("Закрытие модального окна")
     public StudentRegistrationFormPage closeModal() {
         modalButton.click();
         return this;
     }
 
+    @Step("Проверка наличия текста \"{value}\" об успехе заполнения формы")
     public StudentRegistrationFormPage checkMessage(String value) {
         correctMessage.shouldHave(text(value));
         return this;
     }
 
+    @Step("Проверка состояния формы по рамке \"{border}\", цвету \"{color}\" и гендеру \"{female}\"")
     public StudentRegistrationFormPage checkStateForm(String border, String color, String female) {
         nameInput.shouldHave(cssValue(border, color));
         surnameInput.shouldHave(cssValue(border, color));
@@ -152,17 +173,19 @@ public class StudentRegistrationFormPage {
         return this;
     }
 
-
+    @Step("Проверка непустого поля с ключом \"{key}\" и знчением \"{value}\"")
     public StudentRegistrationFormPage checkResultTable(String key, String value) {
         table.checkRow(key, value);
         return this;
     }
 
+    @Step("Проверка пустого значения в ключе \"{key}\"")
     public StudentRegistrationFormPage checkEmptyResultTable(String key) {
         table.checkEmptyRow(key);
         return this;
     }
 
+    @Step("Проверка кликабельности кнопки закрытия модального окна")
     public StudentRegistrationFormPage checkStateModalButton() {
         modalButton.shouldBe(Condition.clickable);
         return this;
--- src/test/java/pages/TextBoxPage.java
+++ src/test/java/pages/TextBoxPage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Condition.cssValue;
 import static com.codeborne.selenide.Condition.text;
@@ -8,7 +9,6 @@ import static com.codeborne.selenide.Selectors.byId;
 import static com.codeborne.selenide.Selenide.*;
 
 public class TextBoxPage {
-    // Elements
     private final SelenideElement userNameInput = $("#userName");
     private final SelenideElement userEmailInput = $("#userEmail");
     private final SelenideElement userCurrentAddressInput = $("#currentAddress");
@@ -16,12 +16,13 @@ public class TextBoxPage {
     private final SelenideElement submitButton = $("#submit");
     private final SelenideElement border = $(".border");
 
-    // Actions
+    @Step("Открыть страницу /text-box")
     public TextBoxPage openPage() {
         open("/text-box");
         return this;
     }
 
+    @Step("Подготовить страницу")
     public TextBoxPage preparePage() {
         executeJavaScript("""
                 document.getElementById('fixedban')?.remove();
@@ -31,51 +32,61 @@ public class TextBoxPage {
         return this;
     }
 
+    @Step("Ввод имени \"{value}\"")
     public TextBoxPage typeUserName(String value) {
         userNameInput.setValue(value);
         return this;
     }
 
+    @Step("Ввод email \"{value}\"")
     public TextBoxPage typeUserEmail(String value) {
         userEmailInput.setValue(value);
         return this;
     }
 
+    @Step("Ввод current address \"{value}\"")
     public TextBoxPage typeCurrentAddress(String value) {
         userCurrentAddressInput.setValue(value);
         return this;
     }
 
+    @Step("Ввод permanent address \"{value}\"")
     public TextBoxPage typePermanentAddress(String value) {
         userPermanentAddressInput.setValue(value);
         return this;
     }
 
+    @Step("Клик по кнопке подтверждения")
     public TextBoxPage clickSubmitButton() {
         submitButton.click();
         return this;
     }
 
+    @Step("Проверка поля \"{key}\" со значением \"{value}\"")
     public TextBoxPage checkUserNameField(String key, String value) {
         $(byId(key)).shouldHave(text(value));
         return this;
     }
 
+    @Step("Проверка поля \"{key}\" со значением \"{value}\"")
     public TextBoxPage checkUserEmailField(String key, String value) {
         $(byId(key)).shouldHave(text(value));
         return this;
     }
 
+    @Step("Проверка поля current address с \"{value}\"")
     public TextBoxPage checkCurrentAddressField(String value) {
         border.find("#currentAddress").shouldHave(text(value));
         return this;
     }
 
+    @Step("Проверка поля permanent address с \"{value}\"")
     public TextBoxPage checkPermanentAddressField(String value) {
         border.find("#permanentAddress").shouldHave(text(value));
         return this;
     }
 
+    @Step("Проверка, что поле ввода email подсветилось красным")
     public TextBoxPage checkUserEmailBorder() {
         userEmailInput.shouldHave((cssValue("border", "1px solid rgb(222, 226, 230)")));
         return this;
